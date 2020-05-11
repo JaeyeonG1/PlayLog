2242dc3977edf572d0d3a894972333e74fd03f2b
yunseo-jeong <ok0102ok0@daum.net>
2020-05-11 16:40:51 +0900

    kggkh

--- app/src/main/java/com/quickids/playlog/activity/Hello.java
+++ app/src/main/java/com/quickids/playlog/activity/Hello.java
@@ -3,4 +3,7 @@ package com.quickids.playlog.activity;
 public class Hello {
     int hu = 0;
     String hi= "hi";
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
 }
